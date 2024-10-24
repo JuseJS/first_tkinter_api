51d8ea6417df480db9d438b138dde8422d0f2dbd
Navarro <joseanp0202@gmail.com>
2024-10-24 08:38:03 +0100

    Modificado cmabiado de screens

--- views/product_screen.py
+++ views/product_screen.py
@@ -1,14 +1,14 @@
 from tkinter import ttk
 
 from views.app_styles import AppStyles
+from views.main_screen import MainScreen
 
 
 class ProductScreen(ttk.Frame):
-        # self.parent_name = parent.winfo_parent()
-        # self.parent = parent.nametowidget(self.parent_name)
-    def __init__(self, parent, *args, **kwargs):
+    def __init__(self, parent, controller, *args, **kwargs):
         super().__init__(parent)
-        self.product = args[1]
+        self.product = args[0]
+        self.controller = controller
 
         # Cargamos los estilos
         AppStyles.configure_styles(self)
@@ -25,10 +25,10 @@ class ProductScreen(ttk.Frame):
         self.search_entry = ttk.Entry(self.main_frame, style="Search.TEntry")
         self.search_entry.place(x=1250, y=20, width=300, height=30)
 
-        btn = ttk.Button(text="Volver Atras", command=self.volver_atras)
+        btn = ttk.Button(self.main_frame, text="Volver Atras", command=self.volver_atras)
         btn.pack()
 
         self.place(x=0, y=0, relwidth=1, relheight=1)
 
     def volver_atras(self):
-        self.main_frame.destroy()
+        self.controller.show_frame(MainScreen)
